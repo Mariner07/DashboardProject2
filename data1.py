a5f89b3775fcddd0f7c2f4b2fd4198fad0f9c734
Hélène Herman <75641605+elanhermi@users.noreply.github.com>
2022-04-01 15:05:03 +0200

    Update data1.py

--- data1.py
+++ data1.py
@@ -3,28 +3,28 @@ import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
+def load_data():
+    data = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
+    return data
+
+data = load_data()
 
 st.title('COVID dashboard Hélène & Maxime & Nurlan')
 
 def simpleGraph():
-  
-  plt.figure(figsize=(14,6))
+  fig=plt.figure(figsize=(14,6))
   plt.title("Death toll")
   plt.xticks(rotation=90)
   plt.xlabel("Date", fontsize=8)
   plt.ylabel("Total deaths per million", fontsize=8)
   sns.lineplot(data=data['total_deaths'])
-  plt.show()
+  return fig
 
 page = st.sidebar.selectbox("Dashboard Options", ("Simple: 1 country", "Complicated"))
 if page== "Simple: 1 country":
-  simpleGraph()
+  st.line_chart(simpleGraph(), use_container_width = True)
   
   
-                           
-
-
 
 st.balloons()
 
