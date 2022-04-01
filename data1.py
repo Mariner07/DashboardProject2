752b6dcd89fee419940444d713160a8494ab313c
Nurlan Nogoibaev <73485842+Mariner07@users.noreply.github.com>
2022-04-01 14:58:10 +0200

    Update data1.py

--- data1.py
+++ data1.py
@@ -9,14 +9,13 @@ st.title('COVID dashboard Hélène & Maxime & Nurlan')
 
 def simpleGraph():
   
-  fig = plt.figure(figsize=(14,6))
+  plt.figure(figsize=(14,6))
   plt.title("Death toll")
   plt.xticks(rotation=90)
   plt.xlabel("Date", fontsize=8)
   plt.ylabel("Total deaths per million", fontsize=8)
   sns.lineplot(data=data['total_deaths'])
   plt.show()
-  return fig
 
 page = st.sidebar.selectbox("Dashboard Options", ("Simple: 1 country", "Complicated"))
 if page== "Simple: 1 country":
