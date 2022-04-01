d6b8aee01e714956b92ffc6176f327150345e28b
Hélène Herman <hlne.herman@gmail.com>
2022-04-01 11:30:04 +0200

    Modifs to make streamlit work

--- data1.py
+++ data1.py
@@ -1,3 +1,6 @@
 import pandas as pd
+import streamlit as st
 
 data = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
+
+st.title('COVID dashboard Hélène & Maxime & Nurlan')
