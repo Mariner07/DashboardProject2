a9a5782dc7e288e9bdfccf6128efe0d251fa0f71
Nurlan Nogoibaev <73485842+Mariner07@users.noreply.github.com>
2022-04-01 15:15:17 +0200

    Update data1.py

--- data1.py
+++ data1.py
@@ -3,11 +3,15 @@ import streamlit as st
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+@st.cache
 def load_data():
     data = pd.read_csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv')
     return data
 
+data_load_state= st.text("Data is in oven")
 data = load_data()
+data_load_state.text("Data is cooked")
+
 
 st.title('COVID dashboard Hélène & Maxime & Nurlan')
 
@@ -22,6 +26,7 @@ def simpleGraph():
 
 page = st.sidebar.selectbox("Dashboard Options", ("Simple: 1 country", "Complicated"))
 if page== "Simple: 1 country":
+    
   st.pyplot(simpleGraph(), use_container_width = True)
   
   
