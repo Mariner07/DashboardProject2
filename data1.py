b14549ff5f5185b19193a4a68e5f5e7a45ab9597
lemaxime <90627609+lemaxime@users.noreply.github.com>
2022-04-01 16:20:41 +0200

    Update data1.py

--- data1.py
+++ data1.py
@@ -18,21 +18,22 @@ data_load_state.text("Data is cooked")
 
 st.title('COVID dashboard Hélène & Maxime & Nurlan')
 
-def simpleGraph():
-  fig=plt.figure(figsize=(14,6))
-  plt.title("Death toll")
-  plt.xticks(rotation=90)
-  plt.xlabel("Date", fontsize=8)
-  plt.ylabel("Total deaths per million", fontsize=8)
-  sns.lineplot(data=data['total_deaths'])
-  return fig
-
 options = st.multiselect(
      'Which countries do you want do display ?',
      data['location'].unique(),
      ['France'])
 
 st.write('You selected:', options)
+
+def simpleGraph(options):
+  fig=plt.figure(figsize=(14,6))
+  plt.title("Death toll")
+  plt.xticks(rotation=90)
+  plt.xlabel("Date", fontsize=8)
+  plt.ylabel("Total deaths per million", fontsize=8)
+  sns.lineplot(data=data[data['location']==options]['total_deaths'])
+  return fig
+
 
 page = st.sidebar.selectbox("Dashboard Options", ("Simple: 1 country", "Complicated"))
 if page== "Simple: 1 country":
