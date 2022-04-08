9be7a24474683eab4c76c99aa845a6cab6bdcdeb
Hélène Herman <75641605+elanhermi@users.noreply.github.com>
2022-04-08 11:42:45 +0200

    Update data1.py

--- data1.py
+++ data1.py
@@ -52,20 +52,20 @@ select_data = st.sidebar.selectbox('Data options', ('Cases', 'Deaths'))
 select_figure = st.sidebar.selectbox('Figure format', ("Number", "Cumulated number", "7 day rolling average"))
 
 if select_data == 'Cases' and select_figure == 'Number':
-    st.plotly_chart(plot(select_figure='new_cases'), use_container_width=True)
+    st.plotly_chart(plot(select_figure='new_cases_per_million'), use_container_width=True)
     
 if select_data == 'Cases' and select_figure == 'Cumulated number':
-    st.plotly_chart(plot(select_figure='total_cases'), use_container_width=True)
+    st.plotly_chart(plot(select_figure='total_cases_per_million'), use_container_width=True)
     
 if select_data == 'Cases' and select_figure == '7 day rolling average':
-    st.plotly_chart(plot(select_figure='new_cases_smoothed'), use_container_width=True)
+    st.plotly_chart(plot(select_figure='new_cases_smoothed_per_million'), use_container_width=True)
     
 if select_data == 'Deaths' and select_figure == 'Number':
-    st.plotly_chart(plot(select_figure='new_deaths'), use_container_width=True)
+    st.plotly_chart(plot(select_figure='new_deaths_per_million'), use_container_width=True)
     
 if select_data == 'Deaths' and select_figure == 'Cumulated number':
-    st.plotly_chart(plot(select_figure='total_deaths'), use_container_width=True)
+    st.plotly_chart(plot(select_figure='total_deaths_per_million'), use_container_width=True)
     
 if select_data == 'Deaths' and select_figure == '7 day rolling average':
-    st.plotly_chart(plot(select_figure='new_deaths_smoothed'), use_container_width=True)
+    st.plotly_chart(plot(select_figure='new_deaths_smoothed_per_million'), use_container_width=True)
     
