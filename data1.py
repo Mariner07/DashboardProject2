ddefb34d69e00aae09880be26fff7c44dd0906cf
lemaxime <90627609+lemaxime@users.noreply.github.com>
2022-04-08 15:49:51 +0200

    Solving for sure I hope

--- data1.py
+++ data1.py
@@ -33,7 +33,7 @@ def filtered_countries(selected_countries_list):
 data_filtered=filtered_countries(country_options)
 
 #Plotting
-def plot(select_figure, title):
+def plot(select_figure):
     fig=px.line(data_filtered, x=data_filtered['date'], y=data_filtered[select_figure], color='location', title=title)
     fig.update_layout(margin={"r": 0, "t": 50, "l": 0, "b": 0})
     return fig
@@ -56,17 +56,17 @@ if select_data == 'Cases' and select_figure == 'Raw number':
     st.plotly_chart(plot(select_figure='new_cases_per_million').update_layout(title='Raw Number of Covid 19 Cases', xaxis_title='Date', yaxis_title='Raw Number of Cases (per million)'), use_container_width=True)
     
 if select_data == 'Cases' and select_figure == 'Cumulated number':
-    st.plotly_chart(plot(select_figure='total_cases_per_million', title = 'Cumulated number of new cases per million'), use_container_width=True)
+    st.plotly_chart(plot(select_figure='total_cases_per_million'), use_container_width=True)
     
 if select_data == 'Cases' and select_figure == '7 day rolling average':
-    st.plotly_chart(plot(select_figure='new_cases_smoothed_per_million', title= '7 day rolling average of new cases per million'), use_container_width=True)
+    st.plotly_chart(plot(select_figure='new_cases_smoothed_per_million'), use_container_width=True)
     
 if select_data == 'Deaths' and select_figure == 'Raw number':
-    st.plotly_chart(plot(select_figure='new_deaths_per_million', title= 'Raw number of deaths per million'), use_container_width=True)
+    st.plotly_chart(plot(select_figure='new_deaths_per_million'), use_container_width=True)
     
 if select_data == 'Deaths' and select_figure == 'Cumulated number':
-    st.plotly_chart(plot(select_figure='total_deaths_per_million', title= 'Cumulated number of deaths per million'), use_container_width=True)
+    st.plotly_chart(plot(select_figure='total_deaths_per_million'), use_container_width=True)
     
 if select_data == 'Deaths' and select_figure == '7 day rolling average':
-    st.plotly_chart(plot(select_figure='new_deaths_smoothed_per_million', title= '7 day rolling average of deaths per million'), use_container_width=True)
+    st.plotly_chart(plot(select_figure='new_deaths_smoothed_per_million'), use_container_width=True)
     
