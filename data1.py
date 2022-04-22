331311b19ecea6456442542a1c46ac4e2a23b7f3
Hélène Herman <hlne.herman@gmail.com>
2022-04-22 09:58:04 +0200

    Fixing date selection

--- data1.py
+++ data1.py
@@ -1,12 +1,4 @@
 import streamlit as st
-st.set_page_config(
-page_title=xxx,
-page_icon=chart_with_upwards_trend,
-layout=“wide”,
-initial_sidebar_state=“auto”,
-menu_items=None,
-)
-
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -21,17 +13,20 @@ def load_data():
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-data_load_state= st.text("Data is in oven")
+data_load_state= st.text("Data is in the oven")
 data = load_data()
 data_load_state.text("Data is cooked")
 
 #Page configuration
-st.title('COVID dashboard HMN & co.')
+st.title('COVID dashboard HMN & co. ☠️')
 
 #Multiselection tool
+not_country_list=['Africa', 'Asia', 'Europe', 'European Union', 'International', 'North America', 'Oceania', 'South America', 'Upper middle income']
+data_with_valid_countries= data[~data['location'].isin(not_country_list)]
+
 country_options = st.multiselect(
      'Which countries do you want do display ?',
-     data['location'].unique(),
+     data_with_valid_countries['location'].unique(),
      ['France'])
 
 def filtered_countries(selected_countries_list):
@@ -40,40 +35,39 @@ def filtered_countries(selected_countries_list):
 
 data_filtered=filtered_countries(country_options)
 
-#Plotting
-def plot(select_figure):
-    fig=px.line(data_filtered, x=data_filtered['date'], y=data_filtered[select_figure], color='location')
-    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-    return fig
-    
 #Date slider
 min_ts = min(data[DATE_COLUMN]).to_pydatetime()
 max_ts = max(data[DATE_COLUMN]).to_pydatetime()
-select_date = pd.to_datetime(st.sidebar.slider("Date range", value=[min_ts, max_ts])
-#st.write(f"Data for {day_date.date()}")
-#data = data[(data['date'] == day_date)]
+select_date = st.sidebar.slider("Date range", value=[min_ts, max_ts])
+data_selected = data_filtered[(data_filtered['date'] > select_date[0]) & (data_filtered['date'] <= select_date[1])]
+
+#Plotting
+def plot(select_figure):
+    fig=px.line(data_selected, x=data_selected['date'], y=data_selected[select_figure], color='location')
+    fig.update_layout(margin={"r": 0, "t": 50, "l": 0, "b": 0})
+    return fig
 
 #Selectbox for data
-select_data = st.sidebar.selectbox('Data options', ('Cases', 'Deaths'))
+select_data = st.sidebar.selectbox('Data options', ('Cases 😷', 'Deaths ⚰️'))
 
 #Selectbox for figure format
 select_figure = st.sidebar.selectbox('Figure format', ("Raw number", "Cumulated number", "7 day rolling average"))
 
-if select_data == 'Cases' and select_figure == 'Raw number':
-    st.plotly_chart(plot(select_figure='new_cases_per_million'), use_container_width=True)
+if select_data == 'Cases 😷' and select_figure == 'Raw number':
+    st.plotly_chart(plot(select_figure='new_cases_per_million').update_layout(title='Raw Number of Covid 19 Cases', xaxis_title='Date', yaxis_title='Raw Number of Cases (per million)'), use_container_width=True)
     
-if select_data == 'Cases' and select_figure == 'Cumulated number':
-    st.plotly_chart(plot(select_figure='total_cases_per_million'), use_container_width=True)
+if select_data == 'Cases 😷' and select_figure == 'Cumulated number':
+    st.plotly_chart(plot(select_figure='total_cases_per_million').update_layout(title='Cumulated Number of Covid 19 Cases', xaxis_title='Date', yaxis_title='Cumulated Number of Cases (per million)'), use_container_width=True)
     
-if select_data == 'Cases' and select_figure == '7 day rolling average':
-    st.plotly_chart(plot(select_figure='new_cases_smoothed_per_million'), use_container_width=True)
+if select_data == 'Cases 😷' and select_figure == '7 day rolling average':
+    st.plotly_chart(plot(select_figure='new_cases_smoothed_per_million').update_layout(title='7 Days Rolling Average of Covid 19 Cases', xaxis_title='Date', yaxis_title='7 Days Rolling Average (per million)'), use_container_width=True)
     
-if select_data == 'Deaths' and select_figure == 'Raw number':
-    st.plotly_chart(plot(select_figure='new_deaths_per_million'), use_container_width=True)
+if select_data == 'Deaths ⚰️' and select_figure == 'Raw number':
+    st.plotly_chart(plot(select_figure='new_deaths_per_million').update_layout(title='Raw Number of Covid 19 Deaths', xaxis_title='Date', yaxis_title='Raw Number of Deaths (per million)'), use_container_width=True)
     
-if select_data == 'Deaths' and select_figure == 'Cumulated number':
-    st.plotly_chart(plot(select_figure='total_deaths_per_million'), use_container_width=True)
+if select_data == 'Deaths ⚰️' and select_figure == 'Cumulated number':
+    st.plotly_chart(plot(select_figure='total_deaths_per_million').update_layout(title='Cumulated Number of Covid 19 Deaths', xaxis_title='Date', yaxis_title='Cumulated Number of Deaths (per million)'), use_container_width=True)
     
-if select_data == 'Deaths' and select_figure == '7 day rolling average':
-    st.plotly_chart(plot(select_figure='new_deaths_smoothed_per_million'), use_container_width=True)
+if select_data == 'Deaths ⚰️' and select_figure == '7 day rolling average':
+    st.plotly_chart(plot(select_figure='new_deaths_smoothed_per_million').update_layout(title='7 Days Rolling Average of Covid 19 Deaths', xaxis_title='Date', yaxis_title='7 Days Rolling Average of Deaths (per million)'), use_container_width=True)
     
