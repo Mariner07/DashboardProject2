ce6e6397282e4333ae36cd07bdd6856381de8a22
Hélène Herman <75641605+elanhermi@users.noreply.github.com>
2022-04-22 10:33:50 +0200

    Update data1.py
    FIXFIXFIX

--- data1.py
+++ data1.py
@@ -39,7 +39,7 @@ data_filtered=filtered_countries(country_options)
 min_ts = min(data[DATE_COLUMN]).to_pydatetime()
 max_ts = max(data[DATE_COLUMN]).to_pydatetime()
 select_date = st.sidebar.slider("Date range", value=[min_ts, max_ts])
-data_selected = data_filtered[(data_filtered['date'] > min_ts) & (data_filtered['date'] <= max_ts)]
+data_selected = data_filtered[(data_filtered['date'] > select_date[0]) & (data_filtered['date'] <= select_date[1])]
 
 #Plotting
 def plot(select_figure):
